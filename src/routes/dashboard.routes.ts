d7a95d3fe3ad867fd36b487b81cef2c43f53a863
matheusmadeira <fumacammadeira@gmail.com>
2026-04-24 19:54:59 -0300

    .

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,17 +1,11 @@
 import { Router } from "express";
 import { DashboardController } from "../controllers/DashboardController";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { roleMiddleware } from "../middlewares/roleMiddleware";
 
 const dashboardRoutes = Router();
 const dashboardController = new DashboardController();
 
-dashboardRoutes.get(
-  "/",
-  authMiddleware,
-  roleMiddleware(["ADMIN"]),
-  dashboardController.getStats
-);
+dashboardRoutes.get("/", authMiddleware, dashboardController.getStats);
 dashboardRoutes.get("/stream", authMiddleware, dashboardController.stream);
 
 export { dashboardRoutes };
